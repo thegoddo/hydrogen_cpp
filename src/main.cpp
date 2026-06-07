a435911224e81bb6369e30f57241bb6531e89eea
theGoddo <biswajitshaw220@gmail.com>
2026-06-07 16:23:36 +0530

    string read from buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-int main(int argc,char* argv[]) {
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+    std::cerr << "hydro <inpyt.hy>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string contents;
+  {
+    std::stringstream contents_stream;
+    std::fstream input(argv[1], std::ios::in);
+    contents_stream << input.rdbuf();
+    contents = contents_stream.str();
+  }
+
+  std::cout << contents << std::endl;
 
 
-  
-  std::cout << argv[1] << std::endl;
-  return 0;
+  return EXIT_FAILURE;
 }
